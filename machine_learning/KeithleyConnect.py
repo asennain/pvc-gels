d2983e9a6204ace76564abde650ca631b2c03de2
Abdul Sennain <asennaini@gmail.com>
2023-07-12 16:04:30 -0700

    Added Keithley functions file

---
+++ machine_learning/KeithleyConnect.py
@@ -0,0 +1,134 @@
+"""***********************************************************
+*** Copyright Tektronix, Inc.                           ***
+*** See www.tek.com/sample-license for licensing terms. ***
+***********************************************************"""
+
+echo_cmd = 1
+
+
+"""*********************************************************************************
+	Function: instrument_connect(my_socket, ip_address string, my_port int) 
+	
+	Purpose: Open an instance of an instrument object for remote communication
+		 over LAN/Ethernet.
+
+	Parameters:
+                my_socket - Instance of a socket object.
+                
+		ip_address (string) - The TCP/IP address string associated with the
+                                      target instrument. 
+		my_port (int) - The instrument connection port. 
+
+	Returns:
+		my_socket - Updated instance of a socket object that includes
+                            attributes of a valid connection. 
+
+	Revisions:
+		2019-07-30    JJB    Initial revision.
+*********************************************************************************"""
+def instrument_connect(my_socket, my_address, my_port, timeout, do_reset, do_id_query):
+    my_socket.connect((my_address, my_port)) # input to connect must be a tuple
+    my_socket.settimeout(timeout)
+    if do_reset == 1:
+        instrument_write(my_socket, "*RST")
+    if do_id_query == 1:
+        tmp_id = instrument_query(my_socket, "*IDN?", 100)
+        print(tmp_id)
+    return my_socket
+
+
+"""*********************************************************************************
+	Function: instrument_disconnect(my_socket)
+	
+	Purpose: Break the LAN/Ethernet connection between the controlling computer
+			 and the target instrument.
+
+	Parameters:
+		my_socket - The TCP instrument connection object used for sending
+							  and receiving data.
+
+	Returns:
+		None
+
+	Revisions:
+		2019-07-30    JJB    Initial revision.
+*********************************************************************************"""
+def instrument_disconnect(my_socket):
+    my_socket.close()
+    return
+
+
+"""*********************************************************************************
+	Function: instrument_write(my_socket, my_command)
+	
+	Purpose: This function issues control commands to the target instrument.
+
+	Parameters:
+		my_socket - The TCP instrument connection object used for sending
+			    and receiving data.
+		my_command (string) - The command issued to the instrument to make it 
+				      perform some action or service. 
+	Returns:
+		None
+
+	Revisions:
+		2019-07-30    JJB    Initial revision.
+*********************************************************************************"""
+def instrument_write(my_socket, my_command):
+    if echo_cmd == 1:
+       my_command
+    cmd = "{0}\n".format(my_command)
+    my_socket.send(cmd.encode())
+    return
+
+
+"""*********************************************************************************
+	Function: instrument_read(my_socket, receive_size)
+	
+	Purpose: This function asks the connected instrument to reply with some
+                 previously requested information, typically queued up from a call
+                 to instrument_write().
+
+	Parameters:
+		my_socket - The TCP instrument connection object used for sending
+			    and receiving data.
+		receive_size (int) - Size of the data/string to be returned to
+                                     the caller. 
+
+	Returns:
+		reply_string (string) - The requested information returned from the 
+					target instrument.
+
+	Revisions:
+		2019-07-30    JJB    Initial revision.
+*********************************************************************************"""
+def instrument_read(my_socket, receive_size):
+    return my_socket.recv(receive_size).decode()
+
+
+"""*********************************************************************************
+	Function: instrument_query(my_socket, my_command, receive_size)
+	
+	Purpose: This function issues control commands to the target instrument with
+                 the expectation that data will be returned. For this function
+                 instance, the returned data is (typically) in string format. 
+
+	Parameters:
+		my_socket - The TCP instrument connection object used for sending
+			    and receiving data.
+		my_command (string) - The command issued to the instrument to make it 
+				      perform some action or service.
+		receive_size (int) - The approximate number of bytes of data the caller
+                                     expects to be returned in the response from the
+                                     instrument. 
+	Returns:
+		reply_string (string) - The requested information returned from the 
+					target instrument. Obtained by way of a caller
+					to instrument_read().
+
+	Revisions:
+		2019-07-30    JJB    Initial revision.
+*********************************************************************************"""
+def instrument_query(my_socket, my_command, receive_size):
+    instrument_write(my_socket, my_command)
+    return instrument_read(my_socket, receive_size)
