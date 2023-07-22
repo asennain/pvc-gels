c92a2f760e9e450f871df16bb3010228713cde1e
Abdul Sennain <asennaini@gmail.com>
2023-07-21 19:29:25 -0700

    Fixed buffer error and temp commands

--- machine_learning/Experiment Automation/functions.py
+++ machine_learning/Experiment Automation/functions.py
@@ -30,7 +30,7 @@ def connect_keithley(ip_address: str, channels: str, data_socket):
     instrument_write(data_socket, ":ROUTe:SCAN:INT .05") # Scan to scan interval 
 
     # choose the buffer and assign all data to the buffer after clearing it
-    instrument_write(data_socket, "TRACe:CLEar") 
+    instrument_write(data_socket, "TRACe:CLEar, \"defbuffer1\"") 
     instrument_write(data_socket, ":TRAC:FILL:MODE CONT, \"defbuffer1\"")
     instrument_write(data_socket, ":ROUT:SCAN:BUFF \"defbuffer1\"")
 
@@ -47,7 +47,7 @@ def connect_keithley(ip_address: str, channels: str, data_socket):
 
     # enable the graph to plot the data
     instrument_write(data_socket, ":DISP:SCR HOME")
-    instrument_write(data_socket, "DISP:WATC:CHAN (@108)")  #!! Hard-coded watch channel 108
+    instrument_write(data_socket, "DISP:WATC:CHAN (@{0})".format(channels)) 
     instrument_write(data_socket, ":DISP:SCR GRAP")
 
 
@@ -64,36 +64,50 @@ def connect_keithley(ip_address: str, channels: str, data_socket):
 def stop_trail_export_csv(file_name, directory_name, data_socket, channels):
     from KeithleyConnect import instrument_query,instrument_write
     import csv
-        
-    channel_count = len(channels.split(','))
 
     while 1 == 1:
-        if int(instrument_query(data_socket, "TRACe:ACTual:END? \"defbuffer1\"", 16).rstrip()) % channel_count == 0:       
-            instrument_write(data_socket,"ABORT")
-            break
         
-    # identify how many readings there are
-    readings_count = int(instrument_query(data_socket, "TRACe:ACTual?", 16).rstrip())
-
-    # preallocate counters and a Data list 
-    start_index = 1
-    end_index = channel_count
-    accumulated_readings = 0
-    Data = []
-
-    # read the buffer and place measurements into the 
-    while accumulated_readings < readings_count:
-        Data.append(instrument_query(data_socket, "TRACe:DATA? {0}, {1}, \"defbuffer1\", REL, READ".format(start_index, end_index), 128).split(','))
-        start_index += channel_count
-        end_index += channel_count
-        accumulated_readings += channel_count
+        readings_count = int(instrument_query(data_socket, "TRACe:ACTual?", 16).rstrip())
         
-    newFile = open('{0} {1}.csv'.format(directory_name, file_name),'w',newline='')
-    newWriter = csv.writer(newFile, dialect='excel')
-    for i in range(len(Data)):
-        if i > 1:
-            newWriter.writerow(Data[i])
-    newFile.close()
+        # preallocate counters and a Data list 
+        start_index = 1
+        end_index = channel_count
+        accumulated_readings = 0
+        Data = []
+
+        # read the buffer and place measurements into the 
+        while accumulated_readings < readings_count:
+            Data.append(instrument_query(data_socket, "TRACe:DATA? {0}, {1}, \"defbuffer1\", REL, READ".format(start_index, end_index), 128).split(','))
+            start_index += channel_count
+            end_index += channel_count
+            accumulated_readings += channel_count
+            
+        newFile = open('{0}{1}.csv'.format(directory_name, file_name),'w',newline='')
+        newWriter = csv.writer(newFile, dialect='excel')
+        for i in range(len(Data)):
+            if i > 1:
+                newWriter.writerow(Data[i])
+        newFile.close()
+
+
+
+
+def step_down(iterations: int, time_interval: int, serial_port, increment_distance: float):
+    """"
+    time_interval is in whole seconds, if for fraction of second 
+    then modify function
+    """
+    import time
+    import serial
+
+    for _ in range(iterations): # argument in range() is the total number of steps
+        serial_port.write("G91\n".encode())
+        time.sleep(1)
+        serial_port.write((f"G1 Z{increment_distance}\n").encode())
+        time.sleep(time_interval - 1) # Total wait time, add one second to number in time.sleep(), so time.sleep(3) is 4 seconds between all
+    
+    serial_port.write((f"G1 Z{-increment_distance * iterations}\n").encode())
+    time.sleep(3)
 
 
 
